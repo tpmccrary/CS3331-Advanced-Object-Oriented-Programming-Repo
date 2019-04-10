613948089cab841801e2a59da0ad29d76e7115c0
Timothy P. McCrary <Timothy P. McCrary@TIM-DESKTOP>
2019-04-10 00:05:04 -0600

    GUI 1st Commit
    Added ItemManager and ItemList classes.

---
+++ Price Watcher/src/edu/utep/cs/cs3331/ItemList.java
@@ -0,0 +1,13 @@
+package edu.utep.cs.cs3331;
+
+import javax.swing.JFrame;
+
+public class ItemList extends ItemView
+{
+	public ItemList()
+	{
+		Item tempItem = new Item("Test", "Test2", 122);
+	}
+	
+	
+}
